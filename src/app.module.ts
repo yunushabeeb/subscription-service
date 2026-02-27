fc69ae333d0d586b638b641ae4598e92a1ce66bb
Yunus Habeeb <habeebdh1@outlook.com>
2026-02-27 14:41:42 +0100

    feat: implement initial structure for admin, auth, subscriptions, users, and webhooks modules with controllers and services

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { SubscriptionsModule } from './subscriptions/subscriptions.module';
+import { WebhooksModule } from './webhooks/webhooks.module';
+import { AdminModule } from './admin/admin.module';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule, UsersModule, AuthModule, SubscriptionsModule, WebhooksModule, AdminModule],
   controllers: [AppController],
   providers: [AppService],
 })
